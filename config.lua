42a7e450533715c9d24040c015676b6a84e8bb29
Vi <nxuaih5@gmail.com>
2025-06-21 20:19:51 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
--- Join https://discord.gg/avsm45ZdgK for full support
+-- Join https://discord.gg/Dx6F8EJ9sD for full support
 
 local libURL = "https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua" -- dont ever change this or the line below else it will break and this is for auto update
 local GAG = loadstring(game:HttpGet(libURL))()
